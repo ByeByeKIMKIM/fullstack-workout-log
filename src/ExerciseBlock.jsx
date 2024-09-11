9cb7f84520f41c71ba951fa1ef2818931c01c5f4
ByeByeKIMKIM <97044938+ByeByeKIMKIM@users.noreply.github.com>
2024-09-10 22:32:55 -0400

    trying to figure out css. trying to figure out diff between index.css and app.css

--- src/ExerciseBlock.jsx
+++ src/ExerciseBlock.jsx
@@ -4,10 +4,10 @@ import styles from './ExerciseBlock.css';
 export default function ExerciseBlock({id, name, sets, reps, onDelete}) {
     return (
         <div className='container'>
+            <button className='exerciseBlockButton' onClick={() => onDelete(id)}>X</button>
             <h2>Exercise: {name}</h2>
             <p>Current Sets: {sets}</p>
             <p>Current Reps: {reps}</p>
-            <button onClick={() => onDelete(id)} style={{ marginTop: '10px', backgroundColor: 'red', color: 'white' }}>Delete Exercise</button>
         </div>
     );
 }
