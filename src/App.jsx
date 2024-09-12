8fe347647b7ab4095b8b5faa7059e787c13d04bd
ByeByeKIMKIM <97044938+ByeByeKIMKIM@users.noreply.github.com>
2024-09-11 21:53:18 -0400

    styling changes

--- src/App.jsx
+++ src/App.jsx
@@ -82,7 +82,7 @@ function App() {
           onDelete = {() => handleDeleteExercise(exercise.id)}
         />
       ))}
-      <button onClick={handleAddExercise}>Add Exercise</button>
+      <button className='addExerciseButton' onClick={handleAddExercise}>Add Exercise</button>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+//createRoot: creates an anchor "root" where App will live
+//getElementById: chooses where the anchor will live
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
